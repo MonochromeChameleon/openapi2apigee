e303317d9e22d449a0305ad6b5652c69daa3dfd7
Hugh Greenish <hugh.greenish@mmc.com>
2022-05-25 09:47:57 +0100

    Don't do cors. We are going to do that better elsewhere later

--- test/commands/generateApiCors.test.js
+++ test/commands/generateApiCors.test.js
@@ -19,58 +19,6 @@ describe('generateApi with CORS proxy', () => {
   });
 
   describe('Add cors policy', () => {
-    it('Cors policy should be generated', async () => {
-      const corsFilePath = path.join(
-        options.destination,
-        `${options.apiProxy}/apiproxy/policies/add-cors.xml`
-      );
-      const corsFile = await fs.lstat(corsFilePath);
-      expect(corsFile.isFile()).toBe(true);
-
-      const corsFileData = await fs.readFile(corsFilePath);
-      const parser = new xml2js.Parser();
-
-      const result = await new Promise((res, rej) => {
-        parser.parseString(corsFileData, (err, r) => {
-          if (err) rej(err);
-          else res(r);
-        });
-      });
-
-      expect(result).toHaveProperty('AssignMessage');
-      expect(result).toHaveProperty('AssignMessage.Add');
-      const headers = result.AssignMessage.Add[0].Headers[0];
-      // Check Header name and value
-      expect(headers.Header[0].$.name).toBe('Access-Control-Allow-Origin');
-      expect(headers.Header[0]._).toBe('*');
-    });
-
-    it('Proxies should contain add-cors step in PreFlow', async () => {
-      const proxiesFilePath = path.join(
-        options.destination,
-        options.apiProxy,
-        '/apiproxy/proxies/default.xml'
-      );
-      const proxiesFileData = await fs.readFile(proxiesFilePath);
-      const parser = new xml2js.Parser();
-
-      const result = await new Promise((res, rej) => {
-        parser.parseString(proxiesFileData, (err, r) => {
-          if (err) rej(err);
-          else res(r);
-        });
-      });
-
-      expect(result).toHaveProperty('ProxyEndpoint');
-      expect(result).toHaveProperty('ProxyEndpoint.PreFlow');
-      expect(result.ProxyEndpoint.PreFlow[0].Response[0].Step[0].Name[0]).toBe(
-        'add-cors'
-      );
-      expect(
-        result.ProxyEndpoint.PreFlow[0].Response[0].Step[0].Condition[0]
-      ).toBe('request.verb != "OPTIONS"');
-    });
-
     it('Proxies should contain noRoute for options request', async () => {
       const proxiesFilePath = path.join(
         options.destination,
@@ -93,32 +41,6 @@ describe('generateApi with CORS proxy', () => {
       expect(result.ProxyEndpoint.RouteRule[0].Condition[0]).toBe(
         'request.verb == "OPTIONS"'
       );
-    });
-
-    it('Proxies should contain OptionsPreFlight step in Flow', async () => {
-      const proxiesFilePath = path.join(
-        options.destination,
-        options.apiProxy,
-        '/apiproxy/proxies/default.xml'
-      );
-      const proxiesFileData = await fs.readFile(proxiesFilePath);
-      const parser = new xml2js.Parser();
-
-      const result = await new Promise((res, rej) => {
-        parser.parseString(proxiesFileData, (err, r) => {
-          if (err) rej(err);
-          else res(r);
-        });
-      });
-
-      expect(result).toHaveProperty('ProxyEndpoint');
-      expect(result).toHaveProperty('ProxyEndpoint.Flows');
-      expect(result.ProxyEndpoint.Flows[0].Flow[0].$.name).toBe(
-        'OptionsPreFlight'
-      );
-      expect(
-        result.ProxyEndpoint.Flows[0].Flow[0].Response[0].Step[0].Name[0]
-      ).toBe('add-cors');
     });
 
     it('Target should not contain header step in PreFlow', async () => {
--- test/commands/generateApiCorsArray.test.js
+++ test/commands/generateApiCorsArray.test.js
@@ -19,52 +19,6 @@ describe('generateApi with CORS proxy (array)', () => {
   });
 
   describe('Add cors policy', () => {
-    it('Cors policy should be generated', async () => {
-      const corsFilePath = path.join(
-        options.destination,
-        `${options.apiProxy}/apiproxy/policies/add-cors.xml`
-      );
-      const corsFile = await fs.lstat(corsFilePath);
-      expect(corsFile.isFile()).toBeTruthy();
-
-      const corsFileData = await fs.readFile(corsFilePath);
-      const parser = new xml2js.Parser();
-      const result = await new Promise((res, rej) => {
-        parser.parseString(corsFileData, (err, r) => {
-          if (err) rej(err);
-          else res(r);
-        });
-      });
-
-      const headers = result.AssignMessage.Add[0].Headers[0];
-      // Check Header name and value
-      expect(headers.Header[0].$.name).toBe('Access-Control-Allow-Origin');
-      expect(headers.Header[0]._).toBe('*');
-    });
-
-    it('Proxies should contain add-cors step in PreFlow', async () => {
-      const proxiesFilePath = path.join(
-        options.destination,
-        options.apiProxy,
-        '/apiproxy/proxies/default.xml'
-      );
-      const proxiesFileData = await fs.readFile(proxiesFilePath);
-      const parser = new xml2js.Parser();
-      const result = await new Promise((res, rej) => {
-        parser.parseString(proxiesFileData, (err, r) => {
-          if (err) rej(err);
-          else res(r);
-        });
-      });
-
-      expect(result.ProxyEndpoint.PreFlow[0].Response[0].Step[0].Name[0]).toBe(
-        'add-cors'
-      );
-      expect(
-        result.ProxyEndpoint.PreFlow[0].Response[0].Step[0].Condition[0]
-      ).toBe('request.verb != "OPTIONS"');
-    });
-
     it('Proxies should contain noRoute for options request', async () => {
       const proxiesFilePath = path.join(
         options.destination,
@@ -85,30 +39,6 @@ describe('generateApi with CORS proxy (array)', () => {
       expect(result.ProxyEndpoint.RouteRule[0].Condition[0]).toBe(
         'request.verb == "OPTIONS"'
       );
-    });
-
-    it('Proxies should contain OptionsPreFlight step in Flow', async () => {
-      const proxiesFilePath = path.join(
-        options.destination,
-        options.apiProxy,
-        '/apiproxy/proxies/default.xml'
-      );
-      const proxiesFileData = await fs.readFile(proxiesFilePath);
-      const parser = new xml2js.Parser();
-
-      const result = await new Promise((res, rej) => {
-        parser.parseString(proxiesFileData, (err, r) => {
-          if (err) rej(err);
-          else res(r);
-        });
-      });
-
-      expect(result.ProxyEndpoint.Flows[0].Flow[0].$.name).toBe(
-        'OptionsPreFlight'
-      );
-      expect(
-        result.ProxyEndpoint.Flows[0].Flow[0].Response[0].Step[0].Name[0]
-      ).toBe('add-cors');
     });
 
     it('Target should not contain header step in PreFlow', async () => {
